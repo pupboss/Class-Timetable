8cd4da34234177b01e8712213aa609b9de85b87a
pupboss <pupboss.vip@gmail.com>
2015-04-25 09:53:22 +0800

    fix some error

--- class-timetable/Tools/Common.h
+++ class-timetable/Tools/Common.h
@@ -6,19 +6,9 @@
 //  Copyright (c) 2014年 PUPBOSS. All rights reserved.
 //
 
-#define MAINURL @"http://api.online.lntu.org/" // 主服务器URL
-#define TOKENURL @"http://eduadm.sinaapp.com/"      // 推送服务器 URL
-#define NOTICEURL @"http://60.18.131.131:11180/academic/jwgg.jsp"   // 教务在线公告地址
-#define WANTEDURL @"https://github.com/LNTUORG/Index"     // 招募地址
-#define PUPBOSSURL @"http://pupboss.com"       //  我的网站
-#define WEIBOURL @"http://www.weibo.com/yuanjingkeji"
-#define STOREURL @"https://itunes.apple.com/cn/app/id955004763"
-
 #define WAITSTR @"正在玩命加载中~~" // 等待提示语
 #define ERRORSTR @"服务器被外星人搬走了o(╯□╰)o" // 错误提示语
 #define NULLSTR @"暂无此类数据"
-#define LJQQ @"10771533"
-#define SUPPORTMAIL @"support@pupboss.com"
 
 #define USERNAMEKEY @"userName"   // 用户名 key
 #define PWDKEY @"pwd"           // 密码 key
@@ -31,10 +21,6 @@
 
 #define TOKENFORNET [[NSUserDefaults standardUserDefaults] objectForKey:LOGINTOKEN]
 
-#define selfInfoFileName @"selfInfoV2.plist"
-#define scheduleFileName @"scheduleV2.plist"
-#define examPlanFileName @"examV2.plist"
-#define selfIconFileName @"selfIcon.png"
 
 // 获得RGB颜色
 #define LJColor(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
